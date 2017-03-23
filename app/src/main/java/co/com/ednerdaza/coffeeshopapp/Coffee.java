afd5985e0c5b942f881bb39fbd9c0d9e6834d718
Cristian Gomez <cristianfgr@gmail.com>
2017-03-23 11:35:26 -0500

    fixes for butterknife usage

---
+++ app/src/main/java/co/com/ednerdaza/coffeeshopapp/Coffee.java
@@ -0,0 +1,25 @@
+package co.com.ednerdaza.coffeeshopapp;
+
+class Coffee {
+
+  //immutability using final variables and not setters
+  private final String name, price, image;
+
+  public Coffee(String name, String price, String image) {
+    this.name = name;
+    this.price = price;
+    this.image = image;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getPrice() {
+    return price;
+  }
+
+  public String getImage() {
+    return image;
+  }
+}
